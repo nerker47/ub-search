b402c8c88598597bba02f13b04a0c70b6c688ade
info@negativetrip.com <info@negativetrip.com@dc15d0ea-f0bd-4f29-89af-2f0a00e7a81f>
2013-10-26 15:35:01 +0000

    update

--- trunk/ub-search/src/test/java/ch/ub/crawler/BasicCrawlControllerTest.java
+++ trunk/ub-search/src/test/java/ch/ub/crawler/BasicCrawlControllerTest.java
@@ -4,21 +4,38 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
+import ch.ub.indexer.ContentRecord;
 import ch.ub.indexer.CrawlContentIndexer;
+import ch.ub.parser.HTMLParser;
 
 public class BasicCrawlControllerTest {
+
+	private final static Logger LOGGER = Logger.getLogger(BasicCrawlControllerTest.class.getName()); 
 
 	@Test
 	public void testStartCrawler() {
 		SitemapParser smParser = new SitemapParser();
+		String searchTerm = "nsa";
 		BasicCrawlController crawlController = new BasicCrawlController();
 		try {
 			List<String> urlList = smParser.getSitemap();
 			crawlController.startCrawler(urlList);
-			CrawlContentIndexer.getInstance().search("nsa");
+			List<ContentRecord> resultsList  = CrawlContentIndexer.getInstance().search(searchTerm);
 			
+			LOGGER.debug("finding similar pages for the resultpages of search term '" + searchTerm + "' " + " results for nsa = " + resultsList.size());
+			
+			for (ContentRecord cr : resultsList)
+			{
+				LOGGER.debug("find similar pages for:" + cr.getUrl());
+				List<ContentRecord> similarResultsList = CrawlContentIndexer.getInstance().likeThis(cr.getUrl());
+				for (ContentRecord scr : similarResultsList)
+				{
+					LOGGER.debug("similar: " + scr.getUrl() + " / " + scr.getTitle());
+				}
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
