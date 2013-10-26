c2aff99e6f155b433ac61dce3fac0e26d04829a0
info@negativetrip.com <info@negativetrip.com@dc15d0ea-f0bd-4f29-89af-2f0a00e7a81f>
2013-10-26 14:30:43 +0000

    update

--- ub-search/src/main/java/ch/ub/indexer/CrawlContentIndexer.java
+++ ub-search/src/main/java/ch/ub/indexer/CrawlContentIndexer.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.management.StandardEmitterMBean;
 
+import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -24,12 +25,17 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.Version;
 
+import ch.ub.parser.HTMLParser;
+
 public class CrawlContentIndexer {
+
+	private final static Logger LOGGER = Logger.getLogger(CrawlContentIndexer.class.getName()); 
 
 	Analyzer analyzer ; 
 	Directory directory ;
 	IndexWriter writer;
 	IndexWriterConfig conf; // = new IndexWriterConfig(analyzer);
+	HTMLParser htmlParser;
 	
 	static CrawlContentIndexer cci = null; 
 	
@@ -47,6 +53,7 @@ public class CrawlContentIndexer {
 		analyzer = new StandardAnalyzer(Version.LUCENE_45); // StandardEmitterMBean(arg0, arg1, arg2) SnowballAnalyzer(Version.LUCENE_45, "German");
 		directory = new RAMDirectory();
 		conf = new IndexWriterConfig(Version.LUCENE_45, analyzer);
+		htmlParser = new HTMLParser();
 		try {
 			writer = new IndexWriter(directory, conf);
 		} catch (IOException e) {
@@ -65,12 +72,13 @@ public class CrawlContentIndexer {
 		}
 	}
 	
-	public void indexContent(String content, String url)
+	public void indexContent(String html, String url)
 	{
-		ContentRecord cr = new ContentRecord();
-		cr.setTitle("");
-		cr.setContent(content);
-		cr.setUrl(url);
+		ContentRecord cr = htmlParser.parse( html, url);
+//		ContentRecord cr = new ContentRecord();
+//		cr.setTitle("");
+//		cr.setContent(content);
+//		cr.setUrl(url);
 		addDocument(cr);
 	}
 	
@@ -82,6 +90,12 @@ public class CrawlContentIndexer {
 		doc.add(new Field("title", title, Field.Store.YES, Field.Index.ANALYZED));  // adding title field
 		String content = contentRecord.getContent();
 		doc.add(new Field("content", content, Field.Store.YES, Field.Index.ANALYZED)); // adding content field
+		String keywords = contentRecord.getMetaKeywords();
+		doc.add(new Field("keywords", content, Field.Store.YES, Field.Index.ANALYZED)); // adding content field
+		String description = contentRecord.getMetaDescription();
+		doc.add(new Field("description", content, Field.Store.YES, Field.Index.ANALYZED)); // adding content field
+		String url = contentRecord.getUrl();
+		doc.add(new Field("url", content, Field.Store.YES, Field.Index.ANALYZED)); // adding content field
 		try {
 			writer.addDocument(doc);
 		} catch (IOException e) {
@@ -90,7 +104,7 @@ public class CrawlContentIndexer {
 		} 
 	}
 	
-	int hitsPerPage = 5;
+	int hitsPerPage = 100;
 	public void search(String querystr) throws IOException, ParseException
 	{
 		IndexReader reader = DirectoryReader.open(directory);
@@ -101,6 +115,7 @@ public class CrawlContentIndexer {
 	    // the "title" arg specifies the default field to use
 	    // when no field is explicitly specified in the query.
 	    Query q = new QueryParser(Version.LUCENE_45, "content", analyzer).parse(querystr);
+
 	    
 		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
 	    searcher.search(q, collector);
@@ -108,10 +123,13 @@ public class CrawlContentIndexer {
 	   
 	    // 4. display results
 	    System.out.println("Found " + hits.length + " hits.");
+	    LOGGER.debug("results for " + querystr);
+	    
+	    
 	    for(int i=0;i<hits.length;++i) {
 	      int docId = hits[i].doc;
 	      Document d = searcher.doc(docId);
-	      System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+	      System.out.println((i + 1) + ". " + d.get("url") + "\t" + d.get("title"));
 	    }
 
 	    // reader can only be closed when there
---
+++ ub-search/src/main/java/ch/ub/parser/HTMLParser.java
@@ -0,0 +1,57 @@
+package ch.ub.parser;
+
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import ch.ub.indexer.ContentRecord;
+
+public class HTMLParser {
+
+	private final static Logger LOGGER = Logger.getLogger(HTMLParser.class.getName()); 
+	public ContentRecord parse(String html, String url)
+	{
+		ContentRecord cr = new ContentRecord();
+		cr.setUrl(url);
+
+		
+		Document doc = Jsoup.parse(html);
+		/*
+		 * <title>48° 8' N, 11° 34' O - Sabina Lorenz | Lyrik | Untergrund-Blättle</title>
+	  
+	  
+      <meta name="description" content="Eine Glastür ist eine Glastür, deshalb darf das Kind sie auch nicht anfassen, wenn die Mutter saugt, spült und wischt.">
+	  
+      <meta name="keywords" content="Untergrund-Blättle, Untergrund, Gesellschaft, Gedichte, 48° 8' N, 11° 34' O, Sabina Lorenz, Prosa, Lyrik, Subkultur, Gegenkultur">
+	  
+		 */
+		
+		Element metatagKeywords = doc.select("meta[name$=keywords]").first(); 
+		Element metatagDescription = doc.select("meta[name$=description]").first(); 		
+		Elements metatags = doc.select("meta[name]"); 
+		Element titleTag = doc.select("title").first();
+		String text = doc.body().text();
+		
+		if (titleTag!=null)
+		{
+		cr.setTitle(titleTag.text());
+		}
+		if (metatagDescription!=null)
+		{
+		cr.setMetaDescription(metatagDescription.attr("content"));
+		}
+		if (metatagKeywords!=null)
+		{
+		cr.setMetaKeywords(metatagKeywords.attr("content"));
+		}
+		if (text!=null)
+		{
+		cr.setContent(text);
+		}
+
+		LOGGER.debug(cr.toString());
+		return cr; 
+	}
+}
