9166e65bfb243a0acb9c913e9b57718333b9bad7
info@negativetrip.com <info@negativetrip.com@dc15d0ea-f0bd-4f29-89af-2f0a00e7a81f>
2014-02-01 15:27:51 +0000

    rewritten js to jsonp

--- ub-search/src/main/webapp/js/similarpages.js
+++ ub-search/src/main/webapp/js/similarpages.js
@@ -1,14 +1,7 @@
-
 var similarpages = {};
 
-similarpages.similarServiceBaseUrl = '/ubsearch/ubsearch';
+similarpages.similarServiceBaseUrl = 'http://192.168.0.34/ubsearch/ubsearch';
 
-similarpages.initialize = function()
-{
-$( ".similarpages" ).each(function( index ) {
-similarpages.showSimilarPages($( this ));
-});
-};
 
 similarpages.showSimilarPages = function(divElement)
 {
@@ -23,32 +16,41 @@ similarpages.getSimilarPages(sourcepage, divElement);
 };
 
 
-similarpages.getSimilarPages = function(sourceUrl, divElement)
-{
 
-      $.ajax({ // ajax call starts
-          url: similarpages.similarServiceBaseUrl, // JQuery loads serverside.php
-          data: 'similar=' + sourceUrl, // Send value of the clicked button
-          dataType: 'json', // Choosing a JSON datatype
-          success: function(data) // Variable data contains the data we get from serverside
-          {
-//				alert(data.similarPages);
-				if (data.similarPages)
-				{
-					for (var i = 0; i < data.similarPages.length; i++) {
-						divElement.append( "<a  class=\"text_box_audio\" href=\"" + data.similarPages[i].url + "\">" + data.similarPages[i].title + "</a>" );
-					}				
-				}
-          }
-      });
 
-}
+
+
+rendersimilarlinks = function(data)
+        {
+        $( ".similarpages" ).each(function( index ) {
+        if (data.similarPages)
+        {
+            for (var i = 0; i < data.similarPages.length; i++)
+                {
+                $( this ).append( "<a  class=\"text_box_audio\" href=\"" + data.similarPages[i].url + "\">" + data.similarPages[i].title + "</a>" );
+                }
+        }
+        });
+        }
+
 
 $(document).ready(function () {
 
+ var x = $( ".similarpages" ).first();
+ 
+ if (x!=undefined)
+        {
+	 var sourcepage = x.data('forpage');
+	 if (sourcepage==undefined)
+	 {
+	 sourcepage=location.pathname;
+	 }	 
 
+        $.ajax({
+            type: 'GET',
+            url: 'http://similar.untergrund-blättle.ch/ubsearch/ubsearch?similar=/archiv/ausgabe2/jugendkriminalitaet.html&callback=rendersimilarlinks&this=mine',
+            dataType: 'jsonp'
+        });
 
-similarpages.initialize();
-
-})
-
+        }
+});
