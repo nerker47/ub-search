793840dd6dbf5a9b2de3d4fcdafeb6200fdc01cb
info@negativetrip.com <info@negativetrip.com@dc15d0ea-f0bd-4f29-89af-2f0a00e7a81f>
2014-02-01 09:44:48 +0000

    updates, externalizing config

--- trunk/ub-search/src/test/java/ch/ub/crawler/SitemapParserTest.java
+++ trunk/ub-search/src/test/java/ch/ub/crawler/SitemapParserTest.java
@@ -7,12 +7,23 @@ import java.util.List;
 
 import org.junit.Test;
 
+import ch.ub.config.Config;
+
 public class SitemapParserTest {
 
 	@Test
 	public void testGetSitemap() {
 		
-		SitemapParser smParser = new SitemapParser();
+		Config config = null;
+		try {
+			config = new Config("config.properties");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String sitempaurl = config.get(Config.CONFIG_PARAM_SITEMAPURL);
+
+		SitemapParser smParser = new SitemapParser(sitempaurl);
 		try {
 			List<String> urlList = smParser.getSitemap();
 			assertNotNull("url list is null", urlList);
